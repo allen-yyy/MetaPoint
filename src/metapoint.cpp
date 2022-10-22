38634228430f144dde8d0533d5d07a5fa5c1e989
allen-yyy <59130189+allen-yyy@users.noreply.github.com>
2022-10-22 19:28:24 +0800

    1

---
+++ src/metapoint.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2022 Allen He
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <iostream>
+
+void
+metapoint_init(int argc,void *argv[])
+{
+    
+    return;
+}
